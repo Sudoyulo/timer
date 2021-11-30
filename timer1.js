55f260f806ac6d6ecd5b6692685f0bfc8b039f65
Kevin Tan Yulo <yulokevin@gmail.com>
2021-11-29 19:47:37 -0800

    timer1

---
+++ timer1.js
@@ -0,0 +1,18 @@
+let input = process.argv.splice(2);
+
+input = input.filter( (x) => (x > 0));
+
+input.sort(function(a, b) {
+  return a - b;
+});
+
+input = input.map((x) => x * 1000);
+console.log(input)
+
+for (let time of input) {
+
+  setTimeout(() => {
+    process.stdout.write('\x07');
+
+  }, time);
+}
